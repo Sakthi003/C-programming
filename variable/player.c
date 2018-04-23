f0f769891c1d5194f9756ce7d576f89dbf905b4a
Sakthi003 <38653034+Sakthi003@users.noreply.github.com>
2018-04-23 10:30:21 +0000

    Create player.c

---
+++ variable/player.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{ 
+  char name[20]="Sachin Ramesh Tendulkar";
+  char nick_name[20]="Little Master";
+  int runs=18426;
+  float avarage=44.83;
+  int matches=463;
+  return 0;
+  
+}
+
